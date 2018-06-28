1674883bfc89713d8426cd8be9227a76e82700d9
lexinamer <lexinamer@gmail.com>
2018-06-28 10:29:09 -0400

    adding media queries

--- wp-content/plugins/rtp-directory/templates/page-directory.php
+++ wp-content/plugins/rtp-directory/templates/page-directory.php
@@ -28,7 +28,7 @@ get_header(); ?>
 							<?php echo do_shortcode('[facetwp facet="industry"]'); ?>
 						</div>
 						<div class="col-sm-6">
-							<div class="row">
+							<div class="row row-smallsize">
 								<div class="col-sm-6">
 									<h3>Facility Types</h3>
 									<?php echo do_shortcode('[facetwp facet="facility_types"]'); ?>
